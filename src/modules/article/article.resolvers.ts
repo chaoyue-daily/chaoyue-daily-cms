6a1cc6b7c14b72d765ab30a925ecd89b10671fc7
yanbing.wang <yanbing.wang@pwc.com>
2019-04-03 16:43:11 +0800

    add article add Mutation and Subscription

--- src/modules/article/article.resolvers.ts
+++ src/modules/article/article.resolvers.ts
@@ -1,11 +1,11 @@
 import { ParseIntPipe, ValidationPipe, UseGuards } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
 import { Int } from 'type-graphql';
-// import { PubSub } from 'graphql-subscriptions';
-import { Article } from '../../graphqlSchema/graphql.schema';
+import { PubSub } from 'graphql-subscriptions';
+import { Article, CreateArticleInput } from '../../graphqlSchema/graphql.schema';
 import { ArticleService } from './article.service';
 
-// const pubSub = new PubSub();
+const pubSub = new PubSub();
 
 @Resolver('Article')
 export class ArticlesResolvers {
@@ -13,7 +13,7 @@ export class ArticlesResolvers {
 
   @Query('getArticles')
   async getArticles(
-    @Args({ name: 'types', type: () => [Int] }) 
+    // @Args({ name: 'types', type: () => [Int] }) 
     types: number[],
   ): Promise<Article[]> {
     return await this.articleService.findAll(types);
@@ -25,5 +25,17 @@ export class ArticlesResolvers {
     id: number,
   ): Promise<Article> {
     return await this.articleService.findOne(id);
+  }
+
+  @Mutation('createArticle')
+  async create(@Args('createArticleInput') args: CreateArticleInput): Promise<Article> {
+    const createdArticle = await this.articleService.create(args);
+    pubSub.publish('articleCreated', { articleCreated: createdArticle });
+    return createdArticle;
+  }
+
+  @Subscription('articleCreated')
+  articleCreated() {
+    return pubSub.asyncIterator('articleCreated');
   }
 }
